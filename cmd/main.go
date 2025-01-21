ed66df33e9d4206bf422d48f534028f2b2257de9
sj-shoff <atryakhin0306@gmail.com>
2025-01-22 00:47:57 +0300

    services + repository

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,17 @@ import (
 	"log"
 	"todo"
 	"todo/pkg/handler"
+	"todo/pkg/repository"
+	"todo/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running hhtp server: %s", err.Error())
+		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
