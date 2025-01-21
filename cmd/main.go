2e535a78e702ce94db710e60fd637fb28cf8730b
sj-shoff <atryakhin0306@gmail.com>
2025-01-21 22:33:19 +0300

    modified:   cmd/main.go 	modified:   go.mod 	new file:   go.sum 	renamed:    pkg/server.go -> server.go 	new file:   todo.go 	new file:   user.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"log"
+	"todo"
+)
+
 func main() {
 	srv := new(todo.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatalf("error occured while running hhtp server: %s", err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module todo
 
 go 1.22.2
+
+require github.com/zhashkevych/todo-app v0.0.0-20210427082504-1789ed69bd5f
