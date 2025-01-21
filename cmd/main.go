2dbed0e991726a1617b06ea8cbb09442c9874369
sj-shoff <atryakhin0306@gmail.com>
2025-01-21 23:59:35 +0300

    handlers

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"todo"
+	"todo/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running hhtp server: %s", err.Error())
 	}
 }
