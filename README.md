de6d5d064daca0406d050e827ed7791afd265305
sj-shoff <atryakhin0306@gmail.com>
2025-01-24 00:19:20 +0300

    docker init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # todo_app
+docker run --name=todo-db -e POSTGRES_PASSWORD='03032006' -p 5436:5432 -d --rm postgres
+docker ps
+миграции - а-ля система контроля версий для баз данных
+import "github.com/golang-migrate/migrate/v4" (библиотека для миграций)
+mkdir -p ./schema
+migrate create -ext sql -dir ./schema -seq init
--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,26 @@ import (
 	"todo/pkg/handler"
 	"todo/pkg/repository"
 	"todo/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
