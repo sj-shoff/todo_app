bd671bc0f67ba1eb2a3d5c055faf80f001b00454
sj-shoff <atryakhin0306@gmail.com>
2025-02-07 21:33:48 +0300

    CRUID items

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -11,7 +11,7 @@ const (
 	todoListsTable  = "todo_lists"
 	usersListsTable = "users_lists"
 	todoItemsTable  = "todo_items"
-	listsItemTable  = "lists_items"
+	listsItemsTable = "lists_items"
 )
 
 type Config struct {
